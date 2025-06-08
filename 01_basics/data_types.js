c4e1f33a0fe61df562e23d5278eb37cee5560323
MaanvendraPrajapati <49813714+MaanvendraPrajapati@users.noreply.github.com>
2025-06-08 08:04:06 +0000

    data types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,21 @@
+const num=100;
+const deci=100.1;
+
+console.log(typeof deci);
+
+const isLoggedIn = false;
+const noValue=null;
+
+console.log(typeof noValue); //object
+
+let name;
+console.log(typeof name);
+
+const hello= function(){
+    console.log("Hello world");
+}
+console.log(typeof hello);
+
+const arr=[1,2,44, "fss"];
+
+console.log(typeof arr);
