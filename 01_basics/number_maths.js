0976ffe5bcfc3095e9a1211b6defdc95b74483c0
MaanvendraPrajapati <49813714+MaanvendraPrajapati@users.noreply.github.com>
2025-06-08 08:55:11 +0000

    string and numbers

---
+++ 01_basics/number_maths.js
@@ -0,0 +1,11 @@
+const number=400
+const num= new Number(500);
+
+//console.log(num);
+//console.log(number);
+
+//console.log(num.toString().length);
+//console.log(num.toFixed(2));
+
+const otherNumber=123.4843;
+//console.log(otherNumber.toPrecision(4));
---
+++ 01_basics/strings.js
@@ -0,0 +1,20 @@
+const name="Maanvendra";
+const repoCount=10;
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const str=new String("Hello World");
+//console.log(str);
+//console.log(str[0]);
+//console.log(str.toUpperCase());
+//console.log(str.charAt(1));
+//console.log(str.indexOf('e'));
+
+const newStr= str.substring(1,5);
+console.log(newStr)
+
+const anotherStr= str.slice(-2);
+console.log(anotherStr);
+
+const flag="Ashish";
+console.log(flag.split(''))
