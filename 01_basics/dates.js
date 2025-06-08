d74e5039c91efd4dc902025d78d1d442ecfcc4aa
MaanvendraPrajapati <49813714+MaanvendraPrajapati@users.noreply.github.com>
2025-06-08 16:31:09 +0000

    basics array

---
+++ 01_basics/dates.js
@@ -0,0 +1,9 @@
+let myDate= new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+const createdDate=new Date(2023,0,12);
+console.log(createdDate);
---
+++ 02_basics/arrays.js
@@ -0,0 +1,5 @@
+const myArr=[1,2,4,"Hello"]
+
+for(let a in myArr){
+    console.log(myArr[a]);
+}
