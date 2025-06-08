7747a945b745d75018983df98bae038b924e787a
MaanvendraPrajapati <49813714+MaanvendraPrajapati@users.noreply.github.com>
2025-06-08 16:06:29 +0000

    random

--- 01_basics/number_maths.js
+++ 01_basics/number_maths.js
@@ -9,3 +9,10 @@ const num= new Number(500);
 
 const otherNumber=123.4843;
 //console.log(otherNumber.toPrecision(4));
+
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
