933274babf0db1b016e5556a7c04bbc30ace16a2
MaanvendraPrajapati <49813714+MaanvendraPrajapati@users.noreply.github.com>
2025-06-10 04:15:02 +0000

    object de-structuring

--- 02_basics/object_adv.js
+++ 02_basics/object_adv.js
@@ -3,10 +3,10 @@ const tinderUser={};
 tinderUser.id=132;
 tinderUser.name="Sam";
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'))
+// console.log(tinderUser.hasOwnProperty('isLogged'))
 
 
 const obj1={1:"a", 2:"b"}
@@ -14,4 +14,16 @@ const obj2={3:"c", 4:"d"}
 
 //const obj3=Object.assign({},obj1, obj2);
 const obj3={...obj1,...obj2};
-console.log(obj3);
+// console.log(obj3);
+
+
+const course={
+    coourse_name:"js",
+    course_instructor: "hitesh"
+}
+
+const{coourse_name}= course
+const{course_instructor: instructor}= course
+
+console.log(instructor)
+console.log(coourse_name)
