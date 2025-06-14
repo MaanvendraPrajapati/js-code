a3e94daf427a0e80608babaa9b4b457d7393e353
MaanvendraPrajapati <49813714+MaanvendraPrajapati@users.noreply.github.com>
2025-06-14 05:34:02 +0000

    more with dates

--- 01_basics/date_adv.js
+++ 01_basics/date_adv.js
@@ -13,4 +13,13 @@ cur_date.setFullYear(2024);
 cur_date.setMonth(0);
 cur_date.setDate(1);
 
-console.log(cur_date);
+console.log(cur_date); //2024-01-01T05:24:21.752Z
+console.log(cur_date.toDateString()); //Mon Jan 01 2024
+console.log(cur_date.toLocaleDateString()) //1/1/2024
+console.log(cur_date.toString()); //Mon Jan 01 2024 05:24:21 GMT+0000 (Coordinated Universal Time)
+console.log(cur_date.toTimeString()); //05:24:21 GMT+0000 (Coordinated Universal Time)
+console.log(cur_date.getTime()); //1704087081908
+
+
+const myTimeStamp=Date.now();
+console.log(myTimeStamp); //Milliseconds 1749878985269
